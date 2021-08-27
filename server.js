09f027413ce373620ecb480ec2f3f486a6a0c9fc
iudayan <iamudayan.9@gmail.com>
2021-08-27 18:08:30 +0530

    update

--- server.js
+++ server.js
@@ -14,16 +14,10 @@ const db = knex({
     database : 'postgres'
   }
 });
-/*
-db.select('*').from('users').then(data=>{
-  console.log(data);
-});*/
-
 
 const app=express();
 app.use(bodyParser.json());
 app.use(cors())
-
 
 app.get('/', (req, res)=> {
   res.send(database.users);
@@ -45,7 +39,7 @@ app.post('/signin', (req, res) => {
         res.status(400).json('wrong credentials')
       }
     })
-    .catch(err => res.status(400).json('wrong credentials'))
+    .catch(err => res.status(400).json('wrong credentials').alert('wrong credentials'))
 })
 
 app.post('/register', (req, res) => {
@@ -103,84 +97,3 @@ app.put('/image', (req, res) => {
 app.listen(3000, ()=> {
   console.log('app is running on port 3000');
 })
-/*
-const express= require('express');
-const bodyParser= require('body-parser');
-const bcrypt = require('bcrypt-nodejs');
-const cors = require('cors');
-const knex = require('knex');
-
-const register=require('./controllers/register');
-const signin=require('./controllers/signin');
-const profile=require('./controllers/profile');
-const image=require('./controllers/image');
-
- const db=knex({
-  client: 'pg',
-  connection: {
-    host : '127.0.0.1',
-    user : 'postgres',
-    password : '123',
-    database : 'smart_brain'
-  },
- 
-});
-/*
-db.select('*').from('users').then(data=>{
-  console.log(data);
-});*
-
-const app=express();
-app.use(bodyParser.json());
-app.use(cors())
-
-/*const database={
-  users:[
-    {
-      id:'123',
-      name:'john',
-      email:'jhon@gmail.com',
-      password:'cookies',
-      entries:0,
-      joined:new Date()
-    },
-    {
-      id:'124',
-      name:'sally',
-      email:'sally@gmail.com',
-      password:'bananas',
-      entries:0,
-      joined:new Date()
-    }
-  ],
-  login:[
-    {
-      id:'987',
-      hash:'',
-
-    }
-  ]
-}
-
-
-app.get('/',(req,res)=>{ res.send(db.users)})
-
-
-app.post('/signin', signin.handleSignin(db, bcrypt))
-
-app.post('/register',(req,res)=>{register.handleRegister(req,res,db,bcrypt) })
-
-
-app.get('/profile/:id',(req,res)=>{profile.handleProfile(req,res,db)})
-
-app.put('/image',(req,res)=>{image.handleImage(req,res,db)})
-
-
-
-
-
-
-app.listen(3000,()=>{
-  console.log('app is running on port 3000')
-})*/
-
